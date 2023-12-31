e8e9ea77360e1383066a18fc4515001e58112350
Imperface <kovzhenko.ua@gmail.com>
2023-12-31 04:54:13 +0200

    clear main

--- utils/index.js
+++
@@ -1,8 +0,0 @@
-const checkDublicateName = (name, data) =>
-  data.find(
-    (item) => item.name.toLowerCase().trim() === name.toLowerCase().trim()
-  );
-
-module.exports = {
-  checkDublicateName,
-};
